7798846873d75053171c0ee387dd3a6a54fe349c
Will Liao <liaotoca@yahoo.com>
2026-02-10 00:53:05 -0500

    Removed jest-setup.js

--- packages/app/jest-setup.js
+++
@@ -1,9 +0,0 @@
-// Make Jest globals available in ESM mode
-// This ensures test(), describe(), etc. work in both JS and TS files
-
-global.test = test;
-global.describe = describe;
-global.expect = expect;
-global.beforeEach = beforeEach;
-global.afterEach = afterEach;
-global.jest = jest;
